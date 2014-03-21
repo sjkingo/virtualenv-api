f64927521314b0ccf9d5ef293e1a4103540903e0
Connor Shearwood <connor@skimlinks.com>
2014-03-21 14:46:16 +0000

    Add a setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='virtualenv-api',
+    version='1.1.0',
+    author='Sam Kingston',
+    author_email='sam@sjkwi.com.au',
+    description='An API for virtualenv/pip',
+    url='https://github.com/sjkingo/virtualenv-api',
+    packages=find_packages(),
+)
