422a7d5d63b1970722786584586375ac857692dd
Sukhikh Roman <roman87.r1s@gmail.com>
2014-08-22 20:49:30 +0400

    python3 compatibility and automatic detection of activated environment

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 
+
 setup(
     name='virtualenv-api',
     version='2.0.0',
@@ -8,5 +9,6 @@ setup(
     author_email='sam@sjkwi.com.au',
     description='An API for virtualenv/pip',
     url='https://github.com/sjkingo/virtualenv-api',
+    install_requires=['six', ],
     packages=find_packages(),
 )
