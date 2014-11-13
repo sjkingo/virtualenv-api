6b6d2eb544f5de642dcdb1f71469d3245a269e4e
Sam Kingston <sam@sjkwi.com.au>
2014-11-14 07:53:37 +1000

    Bump to v2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='virtualenv-api',
-    version='2.0.0',
+    version='2.0.1',
     license='BSD',
     author='Sam Kingston',
     author_email='sam@sjkwi.com.au',
