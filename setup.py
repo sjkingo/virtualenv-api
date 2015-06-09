b3d356652dab367bfd19d5db7f24b62e3d760b4a
Sam Kingston <sam@sjkwi.com.au>
2015-06-10 09:23:32 +1000

    Add README as long_description on pypi. This requires installing pandoc

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     author='Sam Kingston and AUTHORS',
     author_email='sam@sjkwi.com.au',
     description='An API for virtualenv/pip',
+    long_description_markdown_filename='README.md',
     url='https://github.com/sjkingo/virtualenv-api',
+    setup_requires=['setuptools-markdown'],
     install_requires=['six'],
     packages=find_packages(),
 )
